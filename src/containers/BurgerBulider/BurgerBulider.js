03a90fd4d73803d78cee9314e4e318f999f3d148
Gooovip <ghadeer.jarullah@gmail.com>
2020-07-06 18:34:26 +0200

    Startup MyBurger

---
+++ src/containers/BurgerBulider/BurgerBulider.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBulider extends Component {
+  
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Bulid Controles</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBulider;
