2f93044a01ff33d4f5527048e68ab3398d8af588
Gooovip <ghadeer.jarullah@gmail.com>
2020-07-07 18:16:23 +0200

    Burger Component done!

--- src/containers/BurgerBulider/BurgerBulider.js
+++ src/containers/BurgerBulider/BurgerBulider.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
 
 class BurgerBulider extends Component {
-  
+  state = {
+    ingredients: {
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+      salad: 0
+    }
+  };
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients = {this.state.ingredients} />
         <div>Bulid Controles</div>
       </Aux>
     );
